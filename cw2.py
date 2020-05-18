97510ec3bcfc9625e17f103e1cb66b27c84352b2
Adam Sears <adam.sears.1997@gmail.com>
2020-05-18 16:56:31 +0100

    Colour variables, variable names, numbers menu, menu page items, menu changing, gesture handling
    Variables created to hold the RGB values for readability.
    All variable names changed to fit "x_y" format not "xY".
    The numbers menu was created to match a T9 pad.
    Another menu page was added with commands, and the placeholder page had its values entered.
    The system for menu changing when a menu option was selected was implemented.
    Full gesture handling has been created. Currently relies on console to show current code and a single line has been tested to print a number.

--- cw2.py
+++ cw2.py
@@ -2,7 +2,7 @@
 """
 Created on Fri Mar 6 14:47:07 2020
 @author: Dakota Hampson, Adam Sears
-Last edit by Adam Sears on Mon May 18 02:48:36 2020
+Last edit by Adam Sears on Mon May 18 16:47:13 2020
 """
 
 import cv2
@@ -21,6 +21,13 @@ low_wav = sa.WaveObject.from_wave_file('sfx-low.wav')
 
 hand = cv2.CascadeClassifier('haarcascades/Hand.Cascade.1.xml')
 fist = cv2.CascadeClassifier('haarcascades/fist.xml')
+
+white = (255,255,255)
+red = (0,0,255)
+green = (0,255,0)
+blue = (255,0,0)
+orange = (0,128,255)
+dark_blue = (128,0,0)
 
 #Create array of shapes and coordinates
 pts = np.array([[5,240],[105,340],[105,140]], np.int32)
@@ -81,20 +88,28 @@ menu_item_5 = {
 
 menu_shapes = [menu_item_0, menu_item_1, menu_item_2, menu_item_3, menu_item_4, menu_item_5, back_arrow, next_arrow]
 
+numbers_menu = [[205,395,305,475,245,425],[5,5,105,85,45,35],[205,5,305,85,245,35],[405,5,505,85,445,35],[5,135,105,215,45,165],[205,135,305,215,245,165],[405,135,505,215,445,165],[5,265,105,345,45,295],[205,265,305,345,245,295],[405,265,505,345,445,295]]
+
 menu_item = []
-menu_item.append(["if","while","print","number","variable","not"]) #Page 0
-menu_item.append(["if","if","if","if","if","if"]) #Page 1
+menu_item.append(["end line","delete current line","delete last line","backspace","NULL","execute"]) #Page 0
+menu_item.append(["if","while","print","number","variable","not"]) #Page 1
+menu_item.append(["=","+","-",")","]","}"]) #Page 2
 
 menu_page = 0
 action = ""
+change_menu = ""
+
+code = []
+temp_code = ""
+executable_string = ""
 
 #Previous state
-palmsBool = False
-fistsBool = False
+palms_bool = False
+fists_bool = False
 
 #Current count
-palmsCount = 0
-fistsCount = 0
+palms_count = 0
+fists_count = 0
 
 while camera.isOpened():
     #Main Camera
@@ -104,49 +119,66 @@ while camera.isOpened():
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         
         #Draw shapes on camera image
-        cv2.fillPoly(img,[menu_shapes[6]['pts']],(255,0,0))
-        cv2.fillPoly(img,[menu_shapes[7]['pts']],(255,0,0))
-        for i in range(6):
-            img = cv2.rectangle(img,menu_shapes[i]['start'],menu_shapes[i]['end'],(128,0,0),-1)
-            img = cv2.putText(img,menu_item[menu_page][i],menu_shapes[i]['textPos'],cv2.FONT_HERSHEY_DUPLEX,1,(255,255,255))
+        if change_menu == "":
+            cv2.fillPoly(img,[menu_shapes[6]['pts']],blue)
+            cv2.fillPoly(img,[menu_shapes[7]['pts']],blue)
+            for i in range(6):
+                img = cv2.rectangle(img,menu_shapes[i]['start'],menu_shapes[i]['end'],dark_blue,-1)
+                img = cv2.putText(img,menu_item[menu_page][i],menu_shapes[i]['textPos'],cv2.FONT_HERSHEY_DUPLEX,1,white)
+        elif change_menu == "number":
+            for i in range(10):
+                img = cv2.rectangle(img,(numbers_menu[i][0],numbers_menu[i][1]),(numbers_menu[i][2],numbers_menu[i][3]),dark_blue,-1)
+                img = cv2.putText(img,str(i),(numbers_menu[i][4],numbers_menu[i][5]),cv2.FONT_HERSHEY_DUPLEX,1,white)
         
         #Detect body parts
         palms = hand.detectMultiScale(gray, 1.3, 5)
         for (x,y,w,h) in palms:
-            palmsCount += 1 #Counts how many hands have been detected
-            img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,128,255),2)
+            palms_count += 1 #Counts how many hands have been detected
+            img = cv2.rectangle(img,(x,y),(x+w,y+h),orange,2)
         
         fists = fist.detectMultiScale(gray, 1.3, 5)
         for (x,y,w,h) in fists:
-            fistsCount += 1 #Counts how many fists have been detected
-            img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+            fists_count += 1 #Counts how many fists have been detected
+            img = cv2.rectangle(img,(x,y),(x+w,y+h),green,2)
         
         """
         #Gesture detection
         """
         #Palm turns to fist
-        if palmsBool == True and fistsCount > 0:
-            for (xFist,yFist,wFist,hFist) in fists:
-                img = cv2.putText(img,"Palm to Fist",(xFist,yFist),cv2.FONT_HERSHEY_DUPLEX,1,(0,255,0))
-                xFistMiddle = xFist+(wFist/2)
-                yFistMiddle = yFist+(hFist/2)
-                for i in range(len(menu_shapes)):
-                    xShapeStart = menu_shapes[i]['startEnd'][0]
-                    yShapeStart = menu_shapes[i]['startEnd'][1]
-                    xShapeEnd = menu_shapes[i]['startEnd'][2]
-                    yShapeEnd = menu_shapes[i]['startEnd'][3]
-                    if (xFistMiddle > xShapeStart and xFistMiddle < xShapeEnd) and (yFistMiddle > yShapeStart and yFistMiddle < yShapeEnd):
-                        if i < 6:
-                            #Menu option selected    
+        if palms_bool == True and fists_count > 0:
+            for (x_fist,y_fist,w_fist,h_fist) in fists:
+                img = cv2.putText(img,"Palm to Fist",(x_fist,y_fist),cv2.FONT_HERSHEY_DUPLEX,1,green)
+                x_fist_middle = x_fist+(w_fist/2)
+                y_fist_middle = y_fist+(h_fist/2)
+                if change_menu == "":
+                    for i in range(len(menu_shapes)):
+                        x_shape_start = menu_shapes[i]['startEnd'][0]
+                        y_shape_start = menu_shapes[i]['startEnd'][1]
+                        x_shape_end = menu_shapes[i]['startEnd'][2]
+                        y_shape_end = menu_shapes[i]['startEnd'][3]
+                        if (x_fist_middle > x_shape_start and x_fist_middle < x_shape_end) and (y_fist_middle > y_shape_start and y_fist_middle < y_shape_end):
+                            if i < 6:
+                                #Menu option selected    
+                                play_obj = high_wav.play()
+                                action = i
+                            else:
+                                #Arrow selected
+                                play_obj = low_wav.play()
+                                if i == 6:
+                                    action = "back"
+                                elif i == 7:
+                                    action = "next"
+                elif change_menu == "number":
+                    for i in range(10):
+                        x_shape_start = numbers_menu[i][0]
+                        y_shape_start = numbers_menu[i][1]
+                        x_shape_end = numbers_menu[i][2]
+                        y_shape_end = numbers_menu[i][3]
+                        if (x_fist_middle > x_shape_start and x_fist_middle < x_shape_end) and (y_fist_middle > y_shape_start and y_fist_middle < y_shape_end):
+                            #Number selected    
                             play_obj = high_wav.play()
-                            action = i
-                        else:
-                            #Arrow selected
-                            play_obj = low_wav.play()
-                            if i == 6:
-                                action = "back"
-                            elif i == 7:
-                                action = "next"
+                            temp_code += str(i)
+                            change_menu = ""
         """
         #
         """
@@ -164,28 +196,100 @@ while camera.isOpened():
             if menu_page > 0:
                 menu_page -= 1
             else:
-                img = cv2.putText(img,"On first page",(10,30),cv2.FONT_HERSHEY_DUPLEX,1,(0,0,255))
+                img = cv2.putText(img,"On first page",(10,30),cv2.FONT_HERSHEY_DUPLEX,1,red)
                 print("On first page")
         elif action == "next":
             if menu_page < len(menu_item)-1:
                 menu_page += 1
             else:
-                img = cv2.putText(img,"On last page",(10,30),cv2.FONT_HERSHEY_DUPLEX,1,(0,0,255))
+                img = cv2.putText(img,"On last page",(10,30),cv2.FONT_HERSHEY_DUPLEX,1,red)
                 print("On last page")
+        elif action != "":
+            if menu_item[menu_page][action] == "end line":
+                if temp_code != "":
+                    code = code + [temp_code]
+                    temp_code = ""
+                else:
+                    img = cv2.putText(img,"Syntax error",(10,30),cv2.FONT_HERSHEY_DUPLEX,1,red)
+                    print("Syntax error - Cannot end line")
+            elif menu_item[menu_page][action] == "delete current line":
+                temp_code = ""
+            elif menu_item[menu_page][action] == "delete last line":
+                if code:
+                    code.pop()
+                else:
+                    img = cv2.putText(img,"Syntax error",(10,30),cv2.FONT_HERSHEY_DUPLEX,1,red)
+                    print("Syntax error - Cannot delete last line")
+            elif menu_item[menu_page][action] == "backspace":
+                if temp_code != "":
+                    temp_code = temp_code[:-1]
+                else:
+                    img = cv2.putText(img,"Syntax error",(10,30),cv2.FONT_HERSHEY_DUPLEX,1,red)
+                    print("Syntax error - Cannot backspace")
+            elif menu_item[menu_page][action] == "execute":
+                if code:
+                    for i in range(len(code)):
+                        try:
+                            exec(code[i])
+                        except:
+                            print("' " + code[i] + " ' encountered an error")
+                else:
+                    img = cv2.putText(img,"Syntax error",(10,30),cv2.FONT_HERSHEY_DUPLEX,1,red)
+                    print("Syntax error - Cannot execute")
+            elif menu_item[menu_page][action] == "if":
+                if temp_code == "":
+                    temp_code += "if "
+                else:
+                    img = cv2.putText(img,"Syntax error",(10,30),cv2.FONT_HERSHEY_DUPLEX,1,red)
+                    print("Syntax error - Cannot insert if")
+            elif menu_item[menu_page][action] == "while":
+                if temp_code == "":
+                    temp_code += "while "
+                else:
+                    img = cv2.putText(img,"Syntax error",(10,30),cv2.FONT_HERSHEY_DUPLEX,1,red)
+                    print("Syntax error - Cannot insert while")
+            elif menu_item[menu_page][action] == "print":
+                if temp_code == "":
+                    temp_code += "print("
+                else:
+                    img = cv2.putText(img,"Syntax error",(10,30),cv2.FONT_HERSHEY_DUPLEX,1,red)
+                    print("Syntax error - Cannot insert print")
+            elif menu_item[menu_page][action] == "number":
+                change_menu = "number"
+            elif menu_item[menu_page][action] == "variable":
+                change_menu = "variable"
+            elif menu_item[menu_page][action] == "not":
+                temp_code += "not"
+            elif menu_item[menu_page][action] == "=":
+                temp_code += "="
+            elif menu_item[menu_page][action] == "+":
+                temp_code += "+"
+            elif menu_item[menu_page][action] == "-":
+                temp_code += "-"
+            elif menu_item[menu_page][action] == ")":
+                temp_code += ")"
+            elif menu_item[menu_page][action] == "]":
+                temp_code += "]"
+            elif menu_item[menu_page][action] == "}":
+                temp_code += "}"
+            else:
+                img = cv2.putText(img,"Handling error",(10,30),cv2.FONT_HERSHEY_DUPLEX,1,red)
+                print("Error - Command not handled")
         """
         #
         """
-        
+        print(temp_code)
+        print(code)
         """
         #Set variables for next stage of while loop
         """
         #If body part(s) detected set "previous state" boolean to true, if none set to false
-        palmsBool = True if palmsCount > 0 else False
-        fistsBool = True if fistsCount > 0 else False
+        palms_bool = True if palms_count > 0 else False
+        fists_bool = True if fists_count > 0 else False
             
         #Clear current counts
-        palmsCount = 0
-        fistsCount = 0
+        palms_count = 0
+        fists_count = 0
         action = ""
         """
         #
